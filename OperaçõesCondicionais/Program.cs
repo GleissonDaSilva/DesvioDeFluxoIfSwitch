d5e3abc46ece70a3109b9a4cdfb860a84de1af8c
GLEISSON DA SILVA <gleisson.silva@senacsp.edu.br>
2018-04-19 10:26:44 -0300

    Criação OperaçõesCondicionais

---
+++ OperaçõesCondicionais/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OperaçõesCondicionais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
